872df7b8ce8db82abc3566c591b8befedd78e84b
Gerard Fierro <gerrardf7@gmail.com>
2018-01-20 15:50:20 -0600

    Updating Site
    Cleaning of code will be done soon

---
+++ GerardSite/js/GerardSite.js
@@ -0,0 +1,19 @@
+/*This is the javaScript file for Gerard's site
+GerardSite.js
+*/
+
+$(document).ready(function() {
+$(".btn-resume").click(function() {
+ window.location = "Portfolio.html";
+});
+});
+
+$(document).ready(function() {
+    $(".dropdown-toggle").dropdown();
+});
+
+/*
+$("#contact-section").hover(function(){
+    $("#contact-section").animate({up: '20px'});
+});*/
+
