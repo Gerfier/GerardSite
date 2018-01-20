9225cf41164e8a04ab182d4c0b399286bd08439b
Gerard Fierro <gerrardf7@gmail.com>
2018-01-20 15:55:42 -0600

    Fixing commit issues

--- js/GerardSite.js
+++ js/GerardSite.js
@@ -16,3 +16,4 @@ $(document).ready(function() {
 $("#contact-section").hover(function(){
     $("#contact-section").animate({up: '20px'});
 });*/
+
---
+++ js/projectsImg.js
@@ -0,0 +1,19 @@
+
+   /* $(".petUs").click(function(){
+        //open.window("https://rawgit.com/Gerfier/PetUs-Bootstrap/master/index.html");
+        alert("Pet Us Img clicked!");
+    });*/
+
+    $(".petUs").click(function(){
+        window.open("https://rawgit.com/Gerfier/PetUs-Bootstrap/master/index.html");    
+   });
+
+   $(".signIn").click(function(){
+    window.open("https://rawgit.com/Gerfier/LoginPage-Bootstrap/master/index.html");    
+    });
+
+    $(".gallery").click(function(){
+        window.open("file:///C:/Users/Luis/Documents/Programming/Udemy%20WebDev%20Bootcamp/Bootstrap%20Exercises/imageGallery/gallery.html");    
+        });
+
+    
